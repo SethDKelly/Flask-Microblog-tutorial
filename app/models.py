7a60f7a13ca5e1c463c760f5f3c3abd1e092e727
SethDKelly <ollioxenhomefree@gmail.com>
2018-11-19 16:14:51 -0600

    Expand database schema to store blog posts

--- app/models.py
+++ app/models.py
@@ -2,13 +2,24 @@ from app import db
 
 # User class inherits from db.Model
 # db.Model is a base class for all models from Flask_SQLAlchemy
+
 class User(db.Model) :
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), index = True, unique = True)
 	email = db.Column(db.String(120), index = True, unique = True)
 	password_hash = db.Column(db.String(128))
+	posts = db.relationship('Post', backref='author', lazy='dynamic')
 	
 	# this method will tell Python how to print objects of this class
 	# a purpose for this method is when it comes to debugging
 	def __repr__(self) :
 		return '<User {}>'.format(self.username)
+		
+class Post(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	body = db.Column(db.String(140))
+	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	
+	def __repr__(self) :
+		return '<Post {}>'.format(self.body)
