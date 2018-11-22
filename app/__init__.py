953d80104699489f6f5598bec1735f741dd8d019
SethDKelly <ollioxenhomefree@gmail.com>
2018-11-21 19:46:24 -0600

    Add endpoint 'loging' for login required redirects

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,16 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# Setting variable name with LoginManager for the app, flask_login library
 login = LoginManager(app)
+
+# Requiring users to login to view certain pages
+# Allows use of @login_required decorator
+# login_view is set to the name of the view to redirect to
+# This uses dynamic name setting with url_for
+# Absolute naming may also be used '/login'
+login.login_view = 'login'
+
 
 from app import routes, models
